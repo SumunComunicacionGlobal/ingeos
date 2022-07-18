3f7fab4f22a693244ac95d11dc540717d7baa922
Fernando Blasco <fernando@sumun.net>
2022-07-18 08:31:00 +0200

    blog filter collapse

--- global-templates/filtro-blog.php
+++ global-templates/filtro-blog.php
@@ -10,27 +10,35 @@ $queried_obj_id = get_queried_object_id();
 
 if ( $terms ) { ?>
 
-	<nav class="filter-navbar navbar navbar-expand justify-content-center navbar-light mb-2">
+	<nav class="filter-navbar navbar navbar-expand-lg justify-content-center navbar-light mb-2">
 
-		<div class="nav navbar-nav">
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#filter-navbar-collapse" aria-controls="filter-navbar-collapse" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-label mr-1"><?php echo __( 'Por temas', 'kemon' ); ?></span> <span class="navbar-toggler-icon"></span>
+		</button>
 
-			<?php if ( $page_for_posts_id ) { ?>
+		<div class="collapse navbar-collapse" id="filter-navbar-collapse">
 
-				<a class="nav-item nav-link <?php echo $ver_todo_active_class; ?>" href="<?php echo esc_url( get_the_permalink( $page_for_posts_id ) ); ?>"><?php echo __( 'Ver todo', 'smn' ); ?></a>
+			<div class="nav navbar-nav mx-auto">
 
-			<?php } ?>
+				<?php if ( $page_for_posts_id ) { ?>
 
-			<?php foreach ( $terms as $term ) { 
-				$active_class = ( $queried_obj_id == $term->term_id ) ? 'active' : '';
-				?>
+					<a class="nav-item nav-link <?php echo $ver_todo_active_class; ?>" href="<?php echo esc_url( get_the_permalink( $page_for_posts_id ) ); ?>"><?php echo __( 'Ver todo', 'smn' ); ?></a>
 
-				<a class="nav-item nav-link <?php echo $active_class; ?>" href="<?php echo esc_url( get_term_link($term) ); ?>"><?php echo $term->name; ?></a>
+				<?php } ?>
 
-			<?php } ?>
+				<?php foreach ( $terms as $term ) { 
+					$active_class = ( $queried_obj_id == $term->term_id ) ? 'active' : '';
+					?>
 
-			<a class="nav-item nav-link nav-search-button" data-toggle="collapse" href="#search-form" aria-expanded="false" aria-controls="search-form">
-				<?php echo __( 'Buscar', 'smn' ); ?>
-			</a>
+					<a class="nav-item nav-link <?php echo $active_class; ?>" href="<?php echo esc_url( get_term_link($term) ); ?>"><?php echo $term->name; ?></a>
+
+				<?php } ?>
+
+				<a class="nav-item nav-link nav-search-button" data-toggle="collapse" href="#search-form" aria-expanded="false" aria-controls="search-form">
+					<?php echo __( 'Buscar', 'smn' ); ?>
+				</a>
+
+			</div>
 
 		</div>
 
